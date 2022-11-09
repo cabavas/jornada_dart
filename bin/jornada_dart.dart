913bd75823efae598d3ea419f792dbf43189e753
Cabavas <cabavas@gmail.com>
2022-11-09 09:50:33 -0300

    First commit

---
+++ bin/jornada_dart.dart
@@ -0,0 +1,5 @@
+import 'package:jornada_dart/jornada_dart.dart' as jornada_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${jornada_dart.calculate()}!');
+}
---
+++ test/jornada_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:jornada_dart/jornada_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
